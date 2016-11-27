2fe5756bee714e5fadc52707875c6b8ebf058f5e
Jason Roelofs <jasongroelofs@gmail.com>
2016-11-27 12:48:47 -0500

    Configure browserify to run on all javascript files

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,21 @@ module ModernJavascriptAndRails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Configure Browserify to use babelify to compile ES6
+    config.browserify_rails.commandline_options = "-t [ babelify --presets [ es2015 ] ]"
+
+    # Run on all javascript files
+    config.browserify_rails.force = true
+
+    # Alternatively, only run on .es6 files
+    # config.browserify_rails.force = ->(file) { File.extname(file) == ".es6" }
+
+    unless Rails.env.production?
+      # Make sure Browserify is triggered when asked to serve javascript spec files
+      config.browserify_rails.paths << lambda { |p|
+          p.start_with?(Rails.root.join("spec/javascripts").to_s)
+      }
+    end
   end
 end
